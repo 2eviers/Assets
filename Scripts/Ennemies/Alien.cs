0271bd59b6b4c964ec9b6a0ccb13bd9f53dc1449
Loïc Defrance <loic.defrance@gmail.com>
2014-11-22 15:10:23 +0100

    Alien et robot

---
+++ Scripts/Ennemies/Alien.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Alien : Ennemy {
+
+	// Use this for initialization
+	void Start () {
+		IsHiddenScientist = false;
+	}
+}
---
+++ Scripts/Ennemies/Robot.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Robot : Ennemy {
+
+	// Use this for initialization
+	void Start () {
+		IsHiddenScientist = false;
+	}
+}
