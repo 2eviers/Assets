1a00ab3e9b5813ed0ef2534ecf9771e6738828c1
Loïc Defrance <loic.defrance@gmail.com>
2014-11-22 17:16:26 +0100

    Scientifiques gauche
    Placés en fonction de la taille de l'écran

---
+++ Scripts/Ennemies/ScientifiqueGauche.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScientifiqueGauche : MonoBehaviour {
+
+	public float SpawnX;
+
+	void Start () {
+		float PosX = - Camera.main.orthographicSize * Camera.main.aspect + SpawnX;
+		transform.position = new Vector3 (PosX, transform.position.y, transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
