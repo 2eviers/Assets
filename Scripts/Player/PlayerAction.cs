bf20625c6ca91955a820331a46b20078b8f3cdf7
omegavschaos <omegavschaos@gmail.com>
2014-11-22 13:09:32 +0100

    PlayerAction

---
+++ Scripts/Player/PlayerAction.cs
@@ -0,0 +1,61 @@
+﻿using AssemblyCSharp;
+using UnityEngine;
+using System.Collections;
+
+public class PlayerAction : MonoBehaviour
+{
+
+    public GameManager Tete;
+    public GameManager BrasDroit;
+    public GameManager BrasGauche;
+    public GameManager JambeDroite;
+    public GameManager JambeGauche;
+
+    public enum Membre
+    {
+        Tete,BrasDroit,BrasGauche,JambeGauche,JambeDroite
+    }
+
+    public void AddMember(Membre membre, GameManager prefab)
+    {
+        switch (membre)
+        {
+            case Membre.Tete:
+
+                break;
+            case Membre.BrasDroit:
+
+                break;
+            case Membre.BrasGauche:
+
+                break;
+            case Membre.JambeDroite:
+
+                break;
+            case Membre.JambeGauche:
+
+                break;
+        }
+    }
+
+    public void UseCompetence()
+    {
+        Bras bras;
+    }
+
+    public bool UseShield()
+    {
+        return false;
+    }
+
+
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
