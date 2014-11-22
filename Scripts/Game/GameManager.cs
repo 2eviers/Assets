05e1cbf8868ea6617c67597f913c0716aa0048b0
2eviers <pierre.burgmann@gmail.com>
2014-11-22 15:34:59 +0100

    maj GameManager

--- Scripts/Game/GameManager.cs
+++ Scripts/Game/GameManager.cs
@@ -3,14 +3,32 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
+    /**
+     * condition de mort
+     * figer l'écran
+     * afficher le temps
+     * afficher le nb de membres volés
+     * menus
+     * */
+
 	// Use this for initialization
 	void Start ()
 	{
-	    _spawn1 = new Vector3(10, 0, -1);
-        _spawn2 = new Vector3(10, -1.5f, -2);
-        _spawn3 = new Vector3(10, -3, -3);
+	    _spawn1 = new Vector3(15, 0, -1);
+        _spawn2 = new Vector3(15, -1.5f, -2);
+        _spawn3 = new Vector3(15, -3, -3);
 		Speed = 10;
+	    _playerAction = _player.GetComponent<PlayerAction>();
 	}
+
+    [SerializeField] private GameObject _player;
+    private PlayerAction _playerAction;
+
+    void Loose()
+    {
+        //if(true)
+        //Time.timeScale = 0;
+    }
 
     public Vector3 _spawn1 { get; set; }
     public Vector3 _spawn2 { get; set; }
