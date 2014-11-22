9d0d1fd00dd69a7d16850a28eede89689f3fee9a
2eviers <pierre.burgmann@gmail.com>
2014-11-22 16:11:26 +0100

    zone de spawn et mort

--- Scripts/Game/GameManager.cs
+++ Scripts/Game/GameManager.cs
@@ -14,22 +14,24 @@ public class GameManager : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
 	{
-	    _spawn1 = new Vector3(15, 0, -1);
-        _spawn2 = new Vector3(15, -1.5f, -2);
-        _spawn3 = new Vector3(15, -3, -3);
+	    _spawnX = Camera.main.orthographicSize*Camera.main.aspect + 2;
+        _spawn1 = new Vector3(_spawnX, 0, -1);
+        _spawn2 = new Vector3(_spawnX, -1.5f, -2);
+        _spawn3 = new Vector3(_spawnX, -3, -3);
 		Speed = 10;
-	    _playerAction = _player.GetComponent<PlayerAction>();
+	    //_playerAction = _player.GetComponent<PlayerAction>();
 	}
 
     [SerializeField] private GameObject _player;
     private PlayerAction _playerAction;
 
-    void Loose()
-    {
-        //if(true)
-        //Time.timeScale = 0;
-    }
+    //void Loose()
+    //{
+    //    if(_playerAction.IsDead())
+    //        Time.timeScale = 0;
+    //}
 
+    private float _spawnX;
     public Vector3 _spawn1 { get; set; }
     public Vector3 _spawn2 { get; set; }
     public Vector3 _spawn3 { get; set; }
@@ -37,6 +39,6 @@ public class GameManager : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-	
+	    //Loose();
 	}
 }
