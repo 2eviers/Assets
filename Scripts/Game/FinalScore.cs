138355e1aaa92bc12a5b4fa995ecedd50f6b1965
2eviers <pierre.burgmann@gmail.com>
2014-11-23 04:58:33 +0100

    end menu

---
+++ Scripts/Game/FinalScore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FinalScore : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //var time = Math.Round(Time.time, 2);
+        //GetComponent<Text>().text = "Temps : " + time.ToString() +"\n" + "Organes transplantés : " + ;
+	}
+}
