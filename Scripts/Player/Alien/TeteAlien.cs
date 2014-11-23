16d0b2ed5e5c118c786493e0d4cfff3e76c23d27
Loïc Defrance <loic.defrance@gmail.com>
2014-11-23 14:19:07 +0100

    Pouvoir Alien

--- Scripts/Player/Alien/TeteAlien.cs
+++ Scripts/Player/Alien/TeteAlien.cs
@@ -6,5 +6,17 @@ public class TeteAlien : Tete {
 	
 	protected override void ActiveCompetence() {
 		//permet de reveler les scientifiques caches
+		StartCoroutine (AC ());
+	}
+
+	IEnumerator AC() {
+		Ennemy[] EnnemyList = (Ennemy[]) Resources.FindObjectsOfTypeAll (typeof(Ennemy));
+		for (int i = 0; i < 20; i++) {
+			yield return new WaitForSeconds (0.1f);
+			foreach (Ennemy e in EnnemyList) {
+				if (e.IsHiddenScientist)
+					e.renderer.material.color = Color.green;
+			}
+		}
 	}
 }
